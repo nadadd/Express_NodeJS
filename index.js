16a79d16d4d7f3a879821e03db84cde8374d8017
nadadd <104659330+nadadd@users.noreply.github.com>
2023-08-28 13:35:06 +0200

    partie5 video61

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded( {extended: true } ));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
+
+if (app.get('env') === 'development'){
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
+
+app.use(logger);
 
 const courses = [
   {id: 1 , name:'course1'},
@@ -27,7 +43,7 @@ app.get('/api/courses/:id', (req, res) => {
 app.post('/api/courses', (req, res) => {
   const { error } = validateCourse(req.body);
     if (error) return res.status(400).send(result.error.details[0].message);
-    
+
     const course = {
       id: courses.length + 1,
       name: req.body.name
---
+++ logger.js
@@ -0,0 +1,7 @@
+
+function log(req, res, next){
+  console.log('logging...');
+  next();
+};
+
+module.exports = log ;
